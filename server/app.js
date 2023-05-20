cff5de01eca4279caefd8e2c98db9fa6a047b1b4
Mukulkumar <mukul@heliverse.com>
2023-05-20 16:31:12 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors(
     {
-        origin:["http://localhost:3000","https://getmail.netlify.app"]
+        origin:'https://getmail.netlify.app',
+        credentials: true,
     }
 ));
 app.use("/auth",authrouter);
